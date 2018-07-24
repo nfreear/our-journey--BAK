bdf4f119a15a5b4b91aacc1a52935cc59246c14c
Nick Freear <nfreear@yahoo.co.uk>
2018-07-24 13:41:26 +0100

    Bug #24, move forms/editor into separate HTML templates

--- src/assets.js
+++ src/assets.js
@@ -17,8 +17,8 @@ module.exports = {
 const iconFiles = [ {name: 'achievement', file: 'Achievement_card.png'}, {name: 'admin', file: 'Admin_card.png'}, {name: 'assessment', file: 'Assessments_card.png'}, {name: 'communication', file: 'Communication_card.png'}, {name: 'confidence', file: 'ConfidenceBoost_card.png'}, {name: 'duedates', file: 'DueDates_card.png'}, {name: 'helpneeded', file: 'HelpNeeded_card.png'}, {name: 'highpressure', file: 'HighPressure_card.png'}, {name: 'information', file: 'Information_card.png'}, {name: 'lostdirection', file: 'LostDirection_card.png'}, {name: 'lowenergy', file: 'LowEnergy_card.png'}, {name: 'lowscores', file: 'LowScores_card.png'}, {name: 'nosupport', file: 'NoSupport_card.png'}, {name: 'peersupport', file: 'PeerSupport_card.png'}, {name: 'problem', file: 'Problem_card.png'}, {name: 'register', file: 'Register_card.png'}, {name: 'studybreak', file: 'StudyBreak_card.png'}, {name: 'studyexperience', file: 'StudyExperience_card.png'}, {name: 'studygoal', file: 'StudyGoal_card.png'}, {name: 'studymilestone', file: 'StudyMilestone_card.png'}, {name: 'studysuccess', file: 'StudySuccess_card.png'}, {name: 'studysupport', file: 'StudySupport_card.png'}, {name: 'timelost', file: 'TimeLost_card.png'} ];
 const emoticonFiles = [ {name: 'angry', file: 'Angry_emoji.png'}, {name: 'achieve', file: 'Achieve_emoji.png'}, {name: 'bored', file: 'Bored_emoji.png'}, {name: 'confused', file: 'Confused_emoji.png'}, {name: 'excited', file: 'Excited_emoji.png'}, {name: 'happy', file: 'Happy_emoji.png'}, {name: 'nervous', file: 'Nervous_emoji.png'}, {name: 'sick', file: 'Sick_emoji.png'}, {name: 'angry', file: 'Angry_emoji.png'}, {name: 'achieve', file: 'Achieve_emoji.png'}, {name: 'unhappy', file: 'Unhappy_emoji.png'}, {name: 'upset', file: 'Upset_emoji.png'}, {name: 'thinking', file: 'Thinking_emoji.png'} ];
 // var assetDir = 'assets/';
-const emojiDir = 'assets/emoji/';
-const cardDir = 'assets/card/';
+const emojiDir = './assets/emoji/';
+const cardDir = './assets/card/';
 
 function emoticonCount () {
   return emoticonFiles.length;
--- src/layout.js
+++ src/layout.js
@@ -2,16 +2,22 @@
   Layout the SVG journey cards | © 2018 The Open University (IET-OU).
 */
 
-module.exports.reflow = reflow;
+module.exports = {
+  reflow: reflow,
+  setupEditor: setupEditor
+};
 
 const LAYOUTS = require('./layouts.json');
-const SVG_TEMPLATE = document.querySelector('#oj-svg-card-template').innerText;
-const HOLDER = document.querySelector('#journey-canvas .card-holder');
+const SVG_TEMPLATE = require('./views/card-template.svg'); // WAS: document.querySelector('#oj-svg-card-template').innerText;
+// const UPDATE_FORM = require('./views/updateform.html');
+const EDITOR_BAR = require('./views/editorbar.html');
+const OPTIONS_BAR = require('./views/optionsbar.html');
+const CARD_HOLDER = document.querySelector('#journey-canvas .card-holder');
 
 function reflow (layout) {
   layout = layout || 'default';
 
-  console.warn('layout:', layout, LAYOUTS[ layout ], /* SVG_TEMPLATE, */ HOLDER);
+  console.warn('layout:', layout, LAYOUTS[ layout ], /* SVG_TEMPLATE, */ CARD_HOLDER);
 
   let cards = [];
 
@@ -19,7 +25,14 @@ function reflow (layout) {
     cards.push(replaceObj(SVG_TEMPLATE, elem));
   });
 
-  HOLDER.innerHTML = cards.join('\n');
+  CARD_HOLDER.innerHTML = cards.join('\n');
+}
+
+function setupEditor () {
+  // document.querySelector('#updateform').innerHTML = UPDATE_FORM;
+
+  document.querySelector('.editorbar .editorbar-inner').innerHTML = EDITOR_BAR;
+  document.querySelector('.editorbar .optionsbar').innerHTML = OPTIONS_BAR;
 }
 
 // https://github.com/nfreear/gaad-widget/blob/3.x/src/methods.js#L90-L96
